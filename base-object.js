78b6aa015b3a95f0065b2393f16c1ee00b798a59
Dmytro Sylovskyi <dimas09@ukr.net>
2015-03-30 12:27:12 +0300

    fix calling of parent method

--- base-object.js
+++ base-object.js
@@ -5,26 +5,27 @@ function BaseObject() {}
 BaseObject.prototype.init = function() {};
 
 BaseObject.prototype.extend = BaseObject.extend = function(obj) {
+    var instance = new BaseObject();
     for (var k in obj) {
-        if (obj.hasOwnProperty(k)) {
-            if (BaseObject.prototype.hasOwnProperty(k) && typeof obj[k] === 'function') {
-                var base = BaseObject.prototype[k];
-                BaseObject.prototype[k] = obj[k];
-                BaseObject.prototype[k]._base = base;
-            } else {
-                BaseObject.prototype[k] = obj[k];
-            }
+        if (!obj.hasOwnProperty(k)) {
+            continue;
+        }
+        if (this.hasOwnProperty(k) && typeof obj[k] === 'function') {
+            var base = this[k];
+            instance[k] = obj[k];
+            instance[k]._base = base;
+        } else {
+            instance[k] = obj[k];
         }
     }
 
     return function() {
-        var instance = new BaseObject();
         instance.init.apply(instance, arguments);
         return instance;
     };
 };
 
 /* global define:true module:true window: true */
-if (typeof define === 'function' && define['amd'])      { define(function() { return BaseObject; }); } 
-if (typeof module !== 'undefined' && module['exports']) { module['exports'] = BaseObject; } 
+if (typeof define === 'function' && define['amd'])      { define(function() { return BaseObject; }); }
+if (typeof module !== 'undefined' && module['exports']) { module['exports'] = BaseObject; }
 if (typeof window !== 'undefined')                      { window['BaseObject'] = BaseObject; }
